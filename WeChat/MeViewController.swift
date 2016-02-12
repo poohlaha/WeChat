872e731f13aee82beea2d4a28a21ecabe15050ec
smile <smile.love.tao@gmail.com>
2016-02-12 11:40:44 +0800

    添加我的页面

---
+++ WeChat/MeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MeViewController.swift
+//  WeChat
+//
+//  Created by Smile on 16/2/12.
+//  Copyright © 2016年 smile.love.tao@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+//我页面
+class MeViewController: WeChatTableViewNormalController {
+    
+    //MARKS: Properties
+    @IBOutlet weak var myHeaderImageView: UIImageView!//头像
+    @IBOutlet weak var myNameLabel: UILabel!//我的昵称
+    @IBOutlet weak var myWeChatNumLabel: UILabel!//我的微信号
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initFrame()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    //MARKS: 初始化属性
+    func initFrame(){
+        myHeaderImageView.layer.borderWidth = 0.2
+    }
+    
+    
+    //MARKS: tableview cell点击事件
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: false)//取消cell选中事件
+    }
+}
